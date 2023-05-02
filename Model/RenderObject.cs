06c6e837c9208811e6fd772c8ac83dc25d7d173a
Piotr Nieciecki <piotr.nieciecki01@gmail.com>
2023-05-02 12:46:00 +0200

    Architecture scaffold

---
+++ Model/RenderObject.cs
@@ -0,0 +1,17 @@
+﻿using System.Numerics;
+
+namespace _3D_graphics.Model
+{
+    public class RenderObject
+    {
+        private Mesh mesh;
+        private Color color;
+        private Matrix4x4 objectMatrix;
+
+        public RenderObject(Mesh mesh, Color objColor)
+        {
+            this.mesh = mesh;
+            this.color = objColor;
+        }
+    }
+}
