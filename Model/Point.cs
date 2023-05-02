9e064d95fc65c39a68503ea5b64db96768d5f4dd
Piotr Nieciecki <piotr.nieciecki01@gmail.com>
2023-05-02 11:51:32 +0200

    Add project files.

---
+++ Model/Point.cs
@@ -0,0 +1,21 @@
+﻿namespace _3D_graphics.Model
+{
+    public class Point
+    {
+        private float _x;
+        public float x { get { return _x; } set { _x = value; } }
+
+        private float _y;
+        public float y { get { return _y; } set { _y = value; } }
+
+        private float _z;
+        public float z { get { return _z; } set { _z = value; } }
+
+        public Point(float x, float y, float z)
+        {
+            this.x = x;
+            this.y = y;
+            this.z = z;
+        }
+    }
+}
---
+++ View/Form1.cs
@@ -0,0 +1,10 @@
+namespace _3D_graphics
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
