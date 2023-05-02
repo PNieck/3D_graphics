1502c1ba3900ddeddf59d69cedd0eb25ba2c4e83
Piotr Nieciecki <piotr.nieciecki01@gmail.com>
2023-05-02 12:59:45 +0200

    Add car model

--- Controller/SceneInit/InitSceneContoller.cs
+++ Controller/SceneInit/InitSceneContoller.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace _3D_graphics.Controller
+namespace _3D_graphics.Controller.SceneInit
 {
     internal class InitSceneContoller
     {
