346f04928fd456065d935c28123b279e6c88b280
Piotr Nieciecki <piotr.nieciecki01@gmail.com>
2023-05-02 15:12:09 +0200

    Add additional tests for controllers

---
+++ Tests/Controllers/Inits.cs
@@ -0,0 +1,13 @@
+﻿using _3D_graphics.Controller.SceneInit;
+
+namespace Tests.Controllers
+{
+    public class Inits
+    {
+        [Fact]
+        public void InitSceneControllerSuccesfullCreation()
+        {
+            InitSceneContoller controller = new InitSceneContoller();
+        }
+    }
+}
