066ed7fc34d2bc6dde3fc910b5640f66e23dceae
Piotr Nieciecki <piotr.nieciecki01@gmail.com>
2023-05-02 14:55:22 +0200

    Add some tests

---
+++ Tests/ExampleTests.cs
@@ -0,0 +1,15 @@
+﻿namespace Tests
+{
+    public class ExampleTests
+    {
+        [Fact]
+        public void EmptyTest()
+        { }
+
+        [Fact]
+        public void SimpleAssert()
+        {
+            Assert.Equal(2, 1 + 1);
+        }
+    }
+}
--- Tests/UnitTest1.cs
+++ Tests/UnitTest1.cs
@@ -1,11 +1,22 @@
+using _3D_graphics.Controller.SceneInit;
+using _3D_graphics.Model;
+
 namespace Tests
 {
-    public class UnitTest1
+    public class ControllerTests
     {
         [Fact]
-        public void EmptyTest()
+        public void InitSceneControllerSuccesfullCreation()
         {
+            InitSceneContoller controller = new InitSceneContoller();
+        }
 
+        [Fact]
+        public void InitSceneControllerGetABackgroundModels()
+        {
+            InitSceneContoller controller = new InitSceneContoller();
+
+            IEnumerable<RenderObject> objects = controller.GetBackgroundObjects();
         }
     }
 }
