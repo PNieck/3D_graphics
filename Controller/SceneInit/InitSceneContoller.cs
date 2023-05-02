86b401469dcb756b4a5cfc25072a3f8ee2130bdc
Piotr Nieciecki <piotr.nieciecki01@gmail.com>
2023-05-02 13:58:56 +0200

    Add ability to return simple scene

--- Controller/SceneInit/InitSceneContoller.cs
+++ Controller/SceneInit/InitSceneContoller.cs
@@ -1,12 +1,40 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using _3D_graphics.Model;
+using _3D_graphics.Model.Primitives;
 
 namespace _3D_graphics.Controller.SceneInit
 {
-    internal class InitSceneContoller
+    public class InitSceneContoller
     {
+        private ObjParser _objParser;
+
+        public InitSceneContoller()
+        {
+            _objParser = new ObjParser();
+        }
+
+        public IEnumerable<RenderObject> GetBackgroundObjects()
+        {
+            LinkedList<RenderObject> list = new LinkedList<RenderObject>();
+
+            list.AddLast(ParseFromResource(Models.Plane, Color.Green));
+
+            return list;
+        }
+
+        public Car GetCar()
+        {
+            return Car(ParseFromResource(Models.Car, Color.Blue));
+        }
+
+        private RenderObject ParseFromResource(byte[] resources, Color color)
+        {
+            Stream stream = new MemoryStream(Models.Plane);
+            IEnumerable<Mesh> mesh = _objParser.Parse(stream);
+
+            if (mesh.Count() != 1)
+                throw new FileFormatException("Invalid obj file");
+
+            return new RenderObject(mesh.First(), color);
+        }
     }
 }
--- Model/RenderObject.cs
+++ Model/RenderObject.cs
@@ -13,6 +13,10 @@ namespace _3D_graphics.Model
         {
             this.mesh = mesh;
             this.color = objColor;
+
+            objectMatrix = Matrix4x4.Identity;
         }
+
+        public RenderObject(RenderObject renderObject): this(renderObject.mesh, renderObject.color) { }
     }
 }
