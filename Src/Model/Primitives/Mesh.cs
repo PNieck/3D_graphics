8b6f2b9ba5727a49208c33bb2efa5ea51cc09135
Piotr Nieciecki <piotr.nieciecki01@gmail.com>
2023-05-06 15:50:26 +0200

    Update primitives for edge rendering

--- Src/Model/Primitives/Mesh.cs
+++ Src/Model/Primitives/Mesh.cs
@@ -1,12 +1,43 @@
-﻿namespace _3D_graphics.Model.Primitives
-{
+﻿using System.Numerics;
+
+namespace _3D_graphics.Model.Primitives
+{ 
     public class Mesh
     {
-        private List<Triangle> points;
+        private readonly List<Triangle> _originalTriangles;
+        private List<Triangle> _triangles;
+        private Matrix4x4 _objectMatrix;
 
-        public Mesh(IEnumerable<Triangle> triangles)
+        public IEnumerable<Triangle> triangles
+            { get { return _triangles.AsReadOnly(); }  }
+
+        public Mesh(IEnumerable<Triangle> triangles, Vector3 position, float scale = 1)
         {
-            this.points = new List<Triangle>(triangles);
+            _originalTriangles = new List<Triangle>(triangles);
+            _objectMatrix = Matrix4x4.CreateTranslation(position) * Matrix4x4.CreateScale(scale);
+
+            _triangles = new List<Triangle>(_originalTriangles.Count);
+            foreach(Triangle triangle in _originalTriangles)
+            {
+                _triangles.Add(triangle.Transform(_objectMatrix));
+            }
+        }
+
+        public Mesh(IEnumerable<Triangle> triangles) : this(triangles, Vector3.Zero) { }
+
+        public void Scale(float scale)
+        {
+            _objectMatrix *= Matrix4x4.CreateScale(scale);
+            CalculateTriangles();
+        }
+
+
+        private void CalculateTriangles()
+        {
+            for (int i=0; i < _originalTriangles.Count; i++)
+            {
+                _triangles[i] = _originalTriangles[i].Transform(_objectMatrix);
+            }
         }
     }
 }
