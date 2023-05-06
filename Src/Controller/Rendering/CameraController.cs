aa2109f3d77e7b10fd6bd95a6b84a5b202bd3e93
Piotr Nieciecki <piotr.nieciecki01@gmail.com>
2023-05-06 15:48:17 +0200

    Add simple camera implementation

---
+++ Src/Controller/Rendering/CameraController.cs
@@ -0,0 +1,25 @@
+﻿using _3D_graphics.Model;
+using _3D_graphics.Model.Camera;
+using System.Numerics;
+
+namespace _3D_graphics.Controller.Rendering
+{
+    public class CameraController
+    {
+        private BaseCamera _baseCamera;
+
+        public CameraController(Car car, int width, int height) {
+            _baseCamera = new BaseCamera(
+                new Vector3(0, -20, 700), Vector3.Zero, width, height, 100);
+
+            car.AddPositionObserver(TargetMovedHandler);
+        }
+
+        public ICamera GetCamera() => _baseCamera;
+
+        private void TargetMovedHandler(Vector3 newPosition)
+        {
+
+        }
+    }
+}
