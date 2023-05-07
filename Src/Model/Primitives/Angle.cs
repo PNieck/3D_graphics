e40a459d821db2bd9f83eb4496820407b27f91e2
Piotr Nieciecki <piotr.nieciecki01@gmail.com>
2023-05-07 20:29:20 +0200

    Add negation operator

--- Src/Model/Primitives/Angle.cs
+++ Src/Model/Primitives/Angle.cs
@@ -2,10 +2,10 @@
 {
     public struct Angle
     {
-        private static float DEG2RAD = 0.0174532925f;
-        private static float RAD2DEG = 1 / DEG2RAD;
+        private const float DEG2RAD = 0.0174532925f;
+        private const float RAD2DEG = 1 / DEG2RAD;
 
-        private float _degrees;
+        private readonly float _degrees;
 
         public float Degrees { get { return _degrees; } }
         public float Radians { get { return _degrees * DEG2RAD; } }
@@ -17,5 +17,8 @@
 
         public static Angle FromRadians(float radians)
             => new Angle(radians * RAD2DEG);
+
+        public static Angle operator -(Angle angle)
+            => new Angle(-angle._degrees);
     }
 }
